c3e695f43a4a5b4642be7a5024273381c19db5b1
oschre7741 <31477280+oschre7741@users.noreply.github.com>
2018-08-23 12:24:48 -0400

    Update OldMacDonald2.java

--- OldMacDonald2.java
+++ OldMacDonald2.java
@@ -10,26 +10,46 @@
 
 public class OldMacDonald2 
 {
-    
+    /**
+     * Sings the first and fourth lines.
+     *
+     */
     public static void eieio() 
+    
     { 
         System.out.println("Old MacDonald had a farm, E I E I O.");
     }
 
-
+    /**
+     * Sings the second line
+     * 
+     * @param animal for what animal is on the farm
+     * 
+     */
     public static void hadA(String animal) 
     { 
         System.out.println("And on that farm he had a " + animal + ", E I E I O.");
     }
-
-
+    
+    /**
+     * Sings the third line
+     * 
+     * @param sound for what sound the animal makes
+     * 
+     */
     public static void withA(String sound) 
     { 
         System.out.println("With a " + sound + " " + sound +  " here and a " + sound + " " + sound +  " there.");
         System.out.println("Here a " + sound + ", there a " + sound + ", everywhere a " + sound + ", " + sound + ".");  
     }
 
-    
+    /**
+     * Sings the whole verse
+     * 
+     * @param animal for what animal is on the farm
+     * @param sound for what sound the animal makes
+     * 
+     */
     public static void singIt(String animal, String sound) 
     {
         eieio();
@@ -39,7 +59,9 @@ public class OldMacDonald2
         System.out.println();
     }
 
-    
+    /**
+     * Calls the singIt() for each verse.
+     */
     public static void main(String[] args) 
     {
         System.out.println("Old MacDonald Had A Farm\n"); // Give our        song a title
